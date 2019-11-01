b5dd45f9bb6e689d9fe65419f8660699f189f75e
Igor Cavalcanti <igorwc@gmail.com>
2019-11-01 00:07:01 -0300

    adding db support

---
+++ src/config/custom-express.js
@@ -0,0 +1,7 @@
+const express = require('express');
+require('marko/node-require').install();
+require('marko/express');
+const app = express();
+const rotas = require('../app/rotas/rotas.js');
+rotas(app);
+module.exports = app;
