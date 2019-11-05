d147066b5b225bbf1ba1967995952938208542fd
Igor Cavalcanti <igorwc@gmail.com>
2019-11-05 00:15:52 -0300

    adding crud functions

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,4 +1,8 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
+    
     app.get('/', function (req, resp) {
         resp.send(
             `
@@ -14,19 +18,39 @@ module.exports = (app) => {
         );
     });
 
+
     app.get('/livros', function (req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'), {
-                livros: [{
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+
+        const livroDao = new LivroDao(db);
+
+        livroDao.lista( )
+            .then(livros => resp.marko(
+                require('../views/livros/lista/lista.marko'), {
+                    livros
+                }
+
+            ))
+            .catch(erro => console.log(erro));
+
+       
+    });
+
+    app.get('/livros/form', function (req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'))
+    });
+
+    app.post('/livros', function (req, resp) {
+        console.log('requisicao:',req.body);
+        const livroDao = new LivroDao(db);
+        livroDao.adiciona(req.body)
+            .then(resp.redirect('/livros'))
+            .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function (req, resp) { 
+        const livroDao = new LivroDao(db);
+        livroDao.delete(req.params.id)
+            .then(() => resp.status(200).end())
+            .catch(erro => console.log(erro));
     });
 }
--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,7 +1,11 @@
 const express = require('express');
 require('marko/node-require').install();
 require('marko/express');
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+ 
+app.use('/estatico', express.static('src/app/public'));
 const rotas = require('../app/rotas/rotas.js');
 rotas(app);
 module.exports = app;
